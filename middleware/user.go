3130b4808979be3d6428a42c64250731c05812b4
Kenneth Stigen <kenneth.stigen@knowit.no>
2025-01-08 10:30:31 +0100

    Renaming username field

--- middleware/user.go
+++ middleware/user.go
@@ -1,13 +1,14 @@
 package middleware
 
 import (
-	"github.com/gin-gonic/gin"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func UserRequired() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		username := c.Request.Header.Get("X-Auth-Request-Preferred-Username")
+		username := c.Request.Header.Get("X-Preferred-Username")
 		if username == "" {
 			c.JSON(http.StatusForbidden, gin.H{
 				"error": "Not supported without user",
